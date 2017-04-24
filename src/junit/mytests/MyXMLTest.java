d25f256f76635f9a10fdef8aeacd7f6b514dcf41
Alexander Hoffmann <alexander.hoffmann3@haw-hamburg.de>
2017-04-24 20:27:50 +0200

    Junit test erstellt

---
+++ src/junit/mytests/MyXMLTest.java
@@ -0,0 +1,31 @@
+package junit.mytests;
+
+import static org.junit.Assert.*;
+
+import java.lang.reflect.Method;
+
+import org.junit.Test;
+
+import impl.xml.MyXML;
+import impl.xml.MyXMLObject;
+
+public class MyXMLTest {
+
+	@Test
+	public void test() {
+		method("hallo",34);
+	}
+	
+	private void method(String str2, int zahl){
+		Method method = new Object(){}.getClass().getEnclosingMethod();
+		String str = MyXML.createXMLString(method, str2, zahl);
+		MyXMLObject xml = MyXML.createXML(str.getBytes());
+		System.out.println(str);
+		System.out.println(xml.getMethodName());
+		for (int i = 0; i < xml.getParamValues().length; i++) {
+			System.out.println(xml.getParamTypes()[i]);
+			System.out.println(xml.getParamValues()[i]);
+		}
+	}
+
+}
